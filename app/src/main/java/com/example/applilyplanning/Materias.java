3698d02a422ad52ff21182b81b19e09b0e1d201c
nikarmin <nikafworks@gmail.com>
2022-09-05 21:46:47 -0300

    ele pei 🤓

--- app/src/main/java/com/example/applilyplanning/Materias.java
+++ app/src/main/java/com/example/applilyplanning/Materias.java
@@ -2,13 +2,21 @@ package com.example.applilyplanning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class Materias extends AppCompatActivity {
+
+    CalendarView calendario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materias);
+
+        calendario = findViewById(R.id.cvCalendario);
+
+        calendario.setWeekNumberColor(Color.RED);
     }
 }
