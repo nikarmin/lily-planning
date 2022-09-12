3612c040df344a155b4eac62b2ce99703da3730c
NICOLI FERREIRA <u21689@academico.cotuca.unicamp.br>
2022-09-12 11:55:45 -0300

    color picker 🖌

---
+++ app/src/main/java/com/example/applilyplanning/dialog_materias.java
@@ -0,0 +1,47 @@
+package com.example.applilyplanning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class dialog_materias extends AppCompatActivity {
+
+    ImageView wheelColor;
+    View showColor;
+    Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_materias);
+
+        wheelColor = findViewById(R.id.colorPicker);
+        showColor = findViewById(R.id.showColor);
+
+        wheelColor.setDrawingCacheEnabled(true);
+        wheelColor.buildDrawingCache(true);
+
+        wheelColor.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN || motionEvent.getAction() == MotionEvent.ACTION_MOVE){
+                    bitmap = wheelColor.getDrawingCache();
+                    int pixel = bitmap.getPixel((int)motionEvent.getX(), (int)motionEvent.getY());
+
+                    int r = Color.red(pixel);
+                    int g = Color.green(pixel);
+                    int b = Color.blue(pixel);
+
+                    showColor.setBackgroundColor(Color.rgb(r,g,b));
+                }
+                return true;
+            }
+        });
+    }
+}
