ccdf80652e4f44362725bad6158aa6192ed73503
NICOLI FERREIRA <u21689@academico.cotuca.unicamp.br>
2022-09-17 11:53:37 -0300

    adicionando calendário 📅

--- app/src/main/java/com/example/applilyplanning/MainActivity.java
+++ app/src/main/java/com/example/applilyplanning/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dialog_materias);
+        setContentView(R.layout.activity_calendario);
 
     }
 }
