1f243294631f6c8fa7de5fbcd8f1e5edc0c4d099
NICOLI FERREIRA <u21689@academico.cotuca.unicamp.br>
2022-10-03 11:36:22 -0300

    todolist create

--- api/src/controllers/todolistController.js
+++ api/src/controllers/todolistController.js
@@ -2,4 +2,51 @@ require('dotenv/config')
 const { create } = require('domain')
 const { prisma } = require('../database/prismaClient')
 
-module.exports = {}
+module.exports = {
+  async getAll(req, res) {
+    return res.json(
+      await prisma.toDoList.findMany({
+        select: {
+          id_todo_list: true,
+          data_inicio: true,
+          data_entrega: true,
+          nome_lista: true,
+        },
+      })
+    )
+  },
+
+  async getById(req, res) {
+    const { id } = req.params
+
+    return res.json(
+      await prisma.aluno.findUnique({
+        where: { id_aluno: Number(id) },
+        select: {
+          id_aluno: true,
+          nome_aluno: true,
+          email_aluno: true,
+          calendario_aluno: true,
+        },
+      })
+    )
+  },
+
+  async create(req, res) {
+    const { data_inicio, data_entrega, nome_lista, anotacao } = req.body
+
+    const todolist = await prisma.toDoList.create({
+      data: {
+        data_inicio,
+        data_entrega,
+        nome_lista,
+        anotacao,
+      },
+      select: {
+        id_todo_list: true,
+      },
+    })
+
+    return res.status(201).json({ id: todolist.id_todo_list })
+  },
+}
--- api/src/routes.js
+++ api/src/routes.js
@@ -2,6 +2,7 @@
 const { Router } = require('express')
 const alunoController = require('./controllers/alunoController')
 const professorController = require('./controllers/professorController')
+const todolistController = require('./controllers/todolistController')
 
 //const prisma = new PrismaClient();
 
@@ -12,7 +13,6 @@ router.get('/', async (req, res) => {
 })
 
 // alunos
-
 router.get('/alunos', alunoController.getAll)
 router.get('/alunos/:id', alunoController.getById)
 router.post('/alunos', alunoController.create)
@@ -20,11 +20,14 @@ router.post('/alunos/auth', alunoController.auth)
 router.delete('/alunos/:id', alunoController.delete)
 
 // professores
-
 router.get('/professores', professorController.getAll)
 router.get('/professores/:id', professorController.getById)
 router.post('/professores', professorController.create)
 router.post('/professores/auth', professorController.auth)
 router.delete('/professores/:id', professorController.delete)
+
+// todolist
+router.get('/tarefas', todolistController.getAll)
+router.post('/tarefas', alunoController.create)
 
 module.exports = { router }
